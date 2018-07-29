f67bd3c229f002a50c73cbd94a97c57d0d190d26
Qin <245970829@qq.com>
2018-07-29 20:13:49 +0800

    update

---
+++ src/main/scala/cn/njupt/ser/Rules.scala
@@ -0,0 +1,18 @@
+package cn.njupt.ser
+
+import java.net.InetAddress
+
+
+/*object Rules extends Serializable {
+  val rulesMap = Map("hadoop" -> 2.7, "spark" -> 2.2)
+  val hostname = InetAddress.getLocalHost.getHostName
+  println(hostname + "@@@@@@@@@@@@@@@@")
+}*/
+
+
+//第三种方式，希望Rules在EXecutor中被初始化（不走网络了，就不必实现序列化接口）
+object Rules {
+  val rulesMap = Map("hadoop" -> 2.7, "spark" -> 2.2)
+  val hostname = InetAddress.getLocalHost.getHostName
+  println(hostname + "@@@@@@@@@@@@@@@@！！！！")
+}
