06e36509cd9d8626f3bf89724e177016023a6bd0
Qin <245970829@qq.com>
2018-08-30 16:53:36 +0800

    update

--- src/main/scala/cn/njupt/SQL/ScalaUDFExample.scala
+++ src/main/scala/cn/njupt/SQL/ScalaUDFExample.scala
@@ -2,6 +2,7 @@ package cn.njupt.SQL
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.UserDefinedFunction
 
 /**
   * UDF对表中的单行进行转换，以便为每行生成单个对应的输出值
@@ -10,13 +11,15 @@ import org.apache.spark.sql.SparkSession
 object ScalaUDFExample {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Scala UDF Example").setMaster("local[2]")
-    val spark = SparkSession.builder().enableHiveSupport().config(conf).getOrCreate()
+    val spark = SparkSession.builder().config(conf).getOrCreate()
+    import spark.implicits._
 
     val df = spark.read.json("./src/main/resources/temperatures.json")
     df.createOrReplaceTempView("citytemps")
-    spark.udf.register("CTOF", (t: Double) => (t * 9.0 / 5.0) + 32.0)
+    val CTOF: UserDefinedFunction = spark.udf.register("CTOF", (t: Double) => (t * 9.0 / 5.0) + 32.0)
 
-    spark.sql("SELECT city, CTOF(avgLow) AS avgLowF, CTOF(avgHigh) AS avgHighF FROM citytemps").show()
+    df.select($"city", CTOF($"avgLow") as "avgLowF", CTOF($"avgHigh") as "avgHigh").show
+    spark.sql("SELECT city, CTOF(avgLow) AS avgLowF, CTOF(avgHigh) AS avgHighF FROM citytemps").show
 
     spark.stop()
   }
